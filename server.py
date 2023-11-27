47f6a73ba174fc96632af45d2b5175655a81c5b3
Mtrx233 <525541378@qq.com>
2023-11-26 19:18:45 -0600

    实现了Map和Reduce

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import argparse
 import os
 import shutil
+import Settings
+import pickle
 
 def start_server():
     mapper, reducer,file,number = parse_command_line()
@@ -10,11 +12,12 @@ def start_server():
     print(f"file name: %s" % file)
     print(f"number of nodes: %s" % number)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '192.168.1.68'  # 服务器的IP地址，0.0.0.0 表示接受来自任何网络接口的连接
+    # host = '192.168.1.68'  # 服务器的IP地址，0.0.0.0 表示接受来自任何网络接口的连接
+    host = Settings.ServerIP()
     port = 12345       # 选择一个未被占用的端口
 
     server_socket.bind((host, port))
-    server_socket.listen(int(number))  
+    server_socket.listen(int(number))
 
     print(f"Waiting for client to connect {host}:{port}...")
     client = []
@@ -30,13 +33,125 @@ def start_server():
     splitNumber = len(client)
     split_file_by_lines(file,splitNumber,'map_reduce_data')
 
+    # Sending data to reducers
     for i in range(splitNumber):
-        send_file = f"map_reduce_data/reduce_part{i+1}.txt"
-        with open(send_file,'rb') as file:
+        send_file = f"map_reduce_data/reduce_part{i + 1}.txt"
+        with open(send_file, 'rb') as file:
             data = file.read()
-            print(data)
             client[i].client_socket.sendall(data)
+    print("to_be_mapped successfully sent")
+
+    data=None
+    for i in range(len(client)):
+        received_data = client_socket.recv(1024)
+        data = pickle.loads(received_data)
+
+
+    input("按下回车结束")
     shutil.rmtree('map_reduce_data')
+    input("该关闭了")
+
+    # 假装在shuffle
+    dict = {}
+    for iter in data:
+        (key, value) = iter
+        if key not in dict.keys():
+            dict[key] = [1]
+        else:
+            dict[key].append(1)
+
+    # sending to reducers
+    for cli in client:
+        file_to_be_sent = dict
+        file_to_be_sent = pickle.dumps(file_to_be_sent)
+        client_socket.sendall(file_to_be_sent)
+
+    input("准备从reducer接收")
+    sum = 0
+    # receiving from reducers
+    for i in range(len(client)):
+        received_data = client_socket.recv(1024)
+        data = pickle.loads(received_data)
+        sum += data
+
+
+
+    print(sum)
+    input("按下回车结束")
+    shutil.rmtree('map_reduce_data')
+
+
+
+    ########################################################################################################################
+    ########################################################################################################################
+
+    # mapper, reducer, file, number = parse_command_line()
+    # print(f"mapper name: %s" % mapper)
+    # print(f"reducer name: %s" % reducer)
+    # print(f"file name: %s" % file)
+    # print(f"number of nodes: %s" % number)
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # # host = '192.168.1.68'  # 服务器的IP地址，0.0.0.0 表示接受来自任何网络接口的连接
+    # host = Settings.ServerIP()
+    # port = 12345  # 选择一个未被占用的端口
+    #
+    # server_socket.bind((host, port))
+    # server_socket.listen(int(number))
+    #
+    #
+    # for i in range(0, int(number)):
+    #     clientName = f"client_{i}"
+    #     client_socket, client_address = server_socket.accept()
+    #     currentClient = Client(clientName,client_socket, client_address)
+    #     client.append(currentClient)
+    #     print(f"Connected by {client_address}")
+    #
+    # # split the input file into different small part and send to clients
+    # os.makedirs('map_reduce_data')
+    # splitNumber = len(client)
+    # split_file_by_lines(file, splitNumber, 'map_reduce_data')
+    #
+    #
+    # print(f"Waiting for client to connect {host}:{port}...")
+    # ShutdownFlag = False
+    # Clients = []
+    # while(True):    # 循环监听
+    #
+    #     # 关闭客户端连接
+    #     if ShutdownFlag == True:
+    #         client_socket.close()
+    #
+    #     # 等待连接
+    #     client_socket, client_address = server_socket.accept()
+    #     print('连接来自:', client_address)
+    #     currentClient = Client(clientName, client_socket, client_address)
+    #     client.append(currentClient)
+    #     print(f"Connected by {client_address}")
+
+
+
+
+
+
+
+
+def Shuffle(to_be_shuffled):
+    input("Click to shuffle.")
+    shuffled = None
+    return shuffled
+
+def FinalProcess(data):
+    input("Click to finish the final process")
+    result = None
+    return result
+
+# 关闭某个client
+def CloseClient(client, clients):
+    for iter in clients:
+        if iter.address == client.address:
+            clients.remove(iter)
+    return clients
+
 
 
 class Client:
@@ -44,6 +159,11 @@ class Client:
         self.name = name
         self.client_socket = client_socket
         self.client_address = client_address
+        self.state = "idle"
+        # states = ["idle", "mapper", "reducer"]
+
+    def setState(self, state):
+        self.state = state
 
 def split_file_by_lines(input_file, number_of_split,output_dir):
     with open(input_file, 'r') as infile:
---
+++ test.py
@@ -0,0 +1,6 @@
+my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+
+# 将字典转换为列表
+my_list = list(my_dict)
+
+print(my_list)
